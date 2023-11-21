e6eaa06cf0fdb5b62b066134b98b41cf2b17dcd6
BuddyBoyVilla <74051918+BuddyNotFound@users.noreply.github.com>
2023-11-21 17:45:09 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,13 +9,11 @@ this_is_a_map 'yes'
 client_scripts {
     'config.lua',
     'wrapper/cl_wrapper.lua',
-    'wrapper/cl_wp_callback.lua',
     'client/cl_main.lua',
 }
 
 server_scripts {
     'wrapper/sv_wrapper.lua',
-    'wrapper/sv_wp_callback.lua',
     'server/server.lua',
 }
 
