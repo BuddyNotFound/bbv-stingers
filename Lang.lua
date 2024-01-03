93fcd879eff4fc25ed850dc0b5fe92f29cd933db
m1ch433l <153664630+m1ch433l@users.noreply.github.com>
2024-01-03 14:43:09 +0100

    update: Added Lang.SpikesCantBePlaced

--- Lang.lua
+++ Lang.lua
@@ -5,4 +5,5 @@ Lang = {
     SpikesPlaced = "You placed spikes!",
     SpikesPicked = "You picked the spikes!",
     DoingSomething = "You are already placing spikes",
+    SpikesCantBePlaced = "You can't place spikes that far away from you!",
 }
